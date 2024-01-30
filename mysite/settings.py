ddc55e4db3b849ed36d190bd2e4a8217f80f915f
john <johnxyrylpedrosa31@gmail.com>
2024-01-30 14:34:48 +0800

    database

--- mysite/settings.py
+++ mysite/settings.py
@@ -83,11 +83,11 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.environ["PGDATABASE"],
-        'USER': os.environ["PGUSER"],
-        'PASSWORD': os.environ["PGPASSWORD"],
-        'HOST': os.environ["PGHOST"],
-        'PORT': os.environ["PGPORT"],
+        'NAME': os.environ["railway"],
+        'USER': os.environ["postgres"],
+        'PASSWORD': os.environ["2D3b1D-*FDgdAdA4EBba2dfaeeeefFE*"],
+        'HOST': os.environ["roundhouse.proxy.rlwy.net"],
+        'PORT': os.environ["53119"],
     }
 }
 
