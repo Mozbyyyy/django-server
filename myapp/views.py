3ba164378fe1bfea0d26b87e1003e0e439960d29
john <johnxyrylpedrosa31@gmail.com>
2024-01-31 07:36:20 +0800

    31

--- myapp/views.py
+++ myapp/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 
+
 def index(request):
     return render(request, 'index.html')
+
+
+
+
+
 
 
 
--- mysite/settings.py
+++ mysite/settings.py
@@ -82,15 +82,10 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': "railway",
-        'USER': "postgres",
-        'PASSWORD': "2D3b1D-*FDgdAdA4EBba2dfaeeeefFE*",
-        'HOST': "roundhouse.proxy.rlwy.net",
-        'PORT': "53119",
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
