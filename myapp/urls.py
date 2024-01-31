2ce6fbb67aa471f88bcaad4c97c7fdb738beb27e
john <johnxyrylpedrosa31@gmail.com>
2024-01-31 08:00:40 +0800

    123

--- myapp/urls.py
+++ myapp/urls.py
@@ -3,5 +3,5 @@ from .views import index
 
 
 urlpatterns = [
-    path('admin/', index),
+    path('', index),
 ]
--- mysite/settings.py
+++ mysite/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-rc^*w^w&6g9_(uvx#6s*bnt!w)l0rdi%!l7mv#y%uc&x%wo5pk'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["https://django-server-production-40be.up.railway.app/"]
 
@@ -117,13 +117,13 @@ USE_I18N = True
 
 USE_TZ = True
 
-
+STATIC_URL = 'static/'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+# STATIC_URL = 'static/'
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+# STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
