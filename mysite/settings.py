1d21eab26e4c5a349efcf159078009443c099ca8
john <johnxyrylpedrosa31@gmail.com>
2024-01-31 08:35:09 +0800

    333

--- mysite/settings.py
+++ mysite/settings.py
@@ -121,15 +121,16 @@ STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
     BASE_DIR / "static",
-    # "/var/www/static/",
+
 ]
+
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
+
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 # STATIC_URL = 'static/'
 # STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
-
-
-
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
